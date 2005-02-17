2dc3cdc7e48941741a18a6925b3abd129b4a9a2b
hisoka <hisoka@26997cb5-caef-0310-b948-ada137acc39c>
2005-02-17 20:55:25 +0000

    reste a connecter

--- trunk/fabien.h
+++ trunk/fabien.h
@@ -1,8 +1,9 @@
 #ifndef FABIEN_H
 #define FABIEN_H
 
-#include<qmainwindow.h>
-
+#include <qmainwindow.h>
+#include <qimage.h>
+#include "mat_list.h"
 
 
 class Fabien : public QMainWindow
@@ -11,6 +12,9 @@ class Fabien : public QMainWindow
  public:
   Fabien(QWidget *parent=0, const char *name=0);
   ~Fabien();
+
+  bool Est_ligne(QImage *picture, int ord, QRgb noir, int precision);
+  p_liste Liste_lignes(QImage *picture);
 };
 
 #endif
