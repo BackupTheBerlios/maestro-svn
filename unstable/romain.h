3e0345385544b98065f985969dacf6986023575b
hisoka <hisoka@26997cb5-caef-0310-b948-ada137acc39c>
2005-03-29 22:56:50 +0000

    en route vers l'unification

--- unstable/romain.h
+++ unstable/romain.h
@@ -1,68 +1,20 @@
 #ifndef ROMAIN_H
 #define ROMAIN_H
 
-#include<qmainwindow.h>
 #include <qimage.h>
-#include<qfiledialog.h>
+#include <qcheckbox.h>
+#include <qlabel.h>
 
 
-class QVBoxLayout;
-class QHBoxLayout;
-class QGridLayout;
-class QAction;
-class QActionGroup;
-class QToolBar;
-class QPopupMenu;
-class QButtonGroup;
-class QLineEdit;
-class QPushButton;
-class QLabel;
-class QCheckBox;
+// void rom_Charger();
+void rom_FiltrerImage(QImage *img_s, QImage *img_d, float a, float b, float c, float d, float e, float f, float g, float h, float z );
+void rom_Filtrer(QImage img, QImage img_d, QLabel *cadre, QCheckBox *gaussien, QCheckBox *laplacien, QCheckBox *passeh, 
+		 QCheckBox *niveaux, QCheckBox *noir, QCheckBox *selectif, QCheckBox *median);
+void rom_FiltreGris( QImage *img );
+void rom_FiltreNoir( QImage *img );
+void rom_FiltreSelectif( QImage *img_s, QImage *img_d );
+void rom_FiltreMedian( QImage *img_s, QImage *img_d );
 
 
-
-class Romain : public QMainWindow
-{
-  Q_OBJECT
- public:
-  Romain(QWidget *parent=0, const char *name=0);
-  ~Romain();
-  void Charger();
-  void FiltrerImage(QImage *img_s, QImage *img_d, float a, float b, float c, float d, float e, float f, float g, float h, float z );
-  void Filtrer();
-  void FiltreGris( QImage *img );
-  void FiltreNoir( QImage *img );
-  void FiltreSelectif( QImage *img_s, QImage *img_d );
-  void FiltreMedian( QImage *img_s, QImage *img_d );
-
-  QImage img, img_d;
-  float filtre[3][3];
-
-
-  QButtonGroup * MatricePassage;
-  QLineEdit * case1;
-  QLineEdit * case2;
-  QLineEdit * case3;
-  QLineEdit * case4;
-  QLineEdit * case5;
-  QLineEdit * case6;
-  QLineEdit * case7;
-  QLineEdit * case8;
-  QLineEdit * case9;
-  QPushButton * charger;
-  QPushButton * Filter;
-  QLabel * cadre;
-  QButtonGroup * buttonGroup2;
-  QCheckBox * niveaux;
-  QCheckBox * gaussien;
-  QCheckBox * laplacien;
-  QCheckBox * noir;
-  QCheckBox * passeh;
-  QCheckBox * median;
-  QCheckBox * selectif;
-  QToolBar * Toolbar;
-
-
-};
 
 #endif
